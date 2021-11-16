7b0c96ddf0b027d89e9b6068f12ee2b3055c5f0a
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-11-16 17:36:47 +0100

    Guestbook implemented POST

--- app/public/guestbook.php
+++ app/public/guestbook.php
@@ -2,6 +2,23 @@
 
 require_once '../db.php';
 $db = DB::getInstance();
+$GET_ALL_POSTS = 'SELECT id, posted_at, name, email, message, INET_NTOA(ip_address) as ip_address FROM guestbook ORDER BY id DESC';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+    $stmt = $db->prepare("INSERT INTO guestbook 
+        (name, email, message, posted_at, ip_address)
+        VALUES 
+        (:name, :email, :message, now(), :ip_address)");
+    // Only variables can be called pass by reference
+    $ip_address = ip2long($_SERVER["REMOTE_ADDR"]);
+    $stmt->bindParam(':name', $_POST['name']);
+    $stmt->bindParam(':email', $_POST['email']);
+    $stmt->bindParam(':message', $_POST['message']);
+    $stmt->bindParam(':ip_address', $ip_address);
+        
+    $stmt->execute();
+}
 ?>
 
 <html lang="en">
@@ -12,8 +29,29 @@ $db = DB::getInstance();
 <body>
 
 <h2>Guestbook</h2>
+
+<h3>Write something in our guestbook</h3>
+
+<form method="post">
+    <div class="form-field">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name">
+    </div>
+    <div class="form-field">
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email">
+    </div>
+    <div class="form-field">
+        <label for="message">Message:</label>
+        <textarea id="message" name="message"></textarea>
+    </div>
+    <div class="form-field">
+        <label>&nbsp;</label>
+        <input type="submit" value="Send" />
+    </div>
+</form>
 <?php
-$posts = $db->query("SELECT id, posted_at, name, email, message, INET_NTOA(ip_address) as ip_address FROM guestbook");
+$posts = $db->query($GET_ALL_POSTS);
 
 foreach ($posts as $post) {
     ?>
