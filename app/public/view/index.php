67490451c062ce91716de35da5eedb4f78275204
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-11-29 21:31:11 +0100

    Added sql-init script

--- app/public/view/index.php
+++ app/public/view/index.php
@@ -10,10 +10,31 @@
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body class="m-5">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark text-white pb-5">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about">About</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
-<div class="container py-5 bg-light">
+<div class="container bg-light">
+
     <h1 class="h1 text-center">Jack's Guitarshop</h1>
     <h5 class="h5 text-center text-primary">Strings attached</h5>
+    <img src="/img/logo.png" class="img-fluid">
 </div>
 </body>
 </html>
