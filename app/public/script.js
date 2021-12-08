ff60f98090b035d22e693fdebafbc8a9e0c0f84e
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-12-08 09:44:30 +0100

    Added delete-button

--- app/public/script.js
+++ app/public/script.js
@@ -4,8 +4,10 @@ function addItem() {
 
     const container = document.getElementById('itemList');
 
+    const newId = `card-${ container.children.length + 1}`;
     const card = document.createElement('div');
     card.className = 'col-md-6 col-xxl-4 p-1';
+    card.setAttribute('id', newId);
 
     const cardElement = document.createElement('div');
     cardElement.className = 'card';
@@ -20,8 +22,24 @@ function addItem() {
     cardElement.appendChild(cardBody);
     card.appendChild(cardElement);
 
+    const cardFooter = document.createElement('div');
+    cardFooter.className = 'card-footer';
+    const button = document.createElement('button');
+    button.className = 'btn btn-outline-primary';
+    button.setAttribute('onClick', `removeItem(${newId})`);
+
+
+    cardFooter.appendChild(button);
+    card.appendChild(cardFooter);
+
     container.appendChild(card);
 
     textarea.value = '';
+
+}
+
+function removeItem(cardId) {
+    const row = document.getElementById('itemList').children;
+    row.namedItem(cardId).remove();
 
 }
