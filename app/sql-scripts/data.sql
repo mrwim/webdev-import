8fc9a11815ece284e1efb4c04aebecd9e811c26c
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2021-12-11 12:14:36 +0100

    Add initial data to database

--- app/sql-scripts/data.sql
+++ app/sql-scripts/data.sql
@@ -1,73 +1,62 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.6.5-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: mysql
--- Generation Time: Dec 10, 2021 at 04:07 PM
--- Server version: 10.6.5-MariaDB-1:10.6.5+maria~focal
--- PHP Version: 7.4.25
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: developmentdb
+-- ------------------------------------------------------
+-- Server version	10.6.5-MariaDB-1:10.6.5+maria~focal
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `developmentdb`
---
-
--- --------------------------------------------------------
+/*!40103 SET @OLD_TIME_ZONE = @@TIME_ZONE */;
+/*!40103 SET TIME_ZONE = '+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0 */;
+/*!40101 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES = @@SQL_NOTES, SQL_NOTES = 0 */;
 
 --
 -- Table structure for table `guestbook`
 --
 
+DROP TABLE IF EXISTS `guestbook`;
+/*!40101 SET @saved_cs_client = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `guestbook`
 (
-    `id`         int(11) UNSIGNED NOT NULL,
+    `id`         int(11) unsigned NOT NULL AUTO_INCREMENT,
     `posted_at`  datetime         NOT NULL,
     `name`       varchar(255)     NOT NULL,
     `email`      varchar(255)  DEFAULT NULL,
     `message`    varchar(8000) DEFAULT NULL,
-    `ip_address` int(4) UNSIGNED  NOT NULL
+    `ip_address` int(4) unsigned  NOT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
+  AUTO_INCREMENT = 5
   DEFAULT CHARSET = utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `guestbook`
 --
 
-INSERT INTO `guestbook` (`id`, `posted_at`, `name`, `email`, `message`, `ip_address`)
+LOCK TABLES `guestbook` WRITE;
+/*!40000 ALTER TABLE `guestbook`
+    DISABLE KEYS */;
+INSERT INTO `guestbook`
 VALUES (1, '2021-12-10 16:04:03', 'Wim Wiltenburg', 'info@wiltenburg.io', 'This is a test message', 2130706433),
        (2, '2021-12-10 16:04:03', 'Jack Monterey', 'jack.monterrey@gmail.com', 'Another test message', 2130706433);
+/*!40000 ALTER TABLE `guestbook`
+    ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE = @OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `guestbook`
---
-ALTER TABLE `guestbook`
-    ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `guestbook`
---
-ALTER TABLE `guestbook`
-    MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    AUTO_INCREMENT = 3;
-COMMIT;
-
+/*!40101 SET SQL_MODE = @OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES = @OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-10 16:53:00
